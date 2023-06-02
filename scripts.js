25d4aaac6c48b4e62bfa9cd810e2ac1bec2758cb
Александр Озимлевский <alex@MacBook-Air-Aleksandr.local>
2023-06-02 19:09:36 +0300

    origin

---
+++ scripts.js
@@ -0,0 +1,57 @@
+let yearOfBirth = prompt("Введіть рік вашого народження:");
+if (yearOfBirth === null) {
+  alert("Шкода, що Ви не захотіли ввести свій рік народження.");
+} else {
+  let city = prompt("Введіть місто, в якому ви живете:");
+  if (city === null) {
+    alert("Шкода, що Ви не захотіли ввести своє місто.");
+  } else {
+    let sport = prompt("Введіть ваш улюблений вид спорту:");
+    if (sport === null) {
+      alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
+    } else {
+      let currentYear = new Date().getFullYear();
+      let age = currentYear - parseInt(yearOfBirth);
+
+      let message = "Ваші дані:\n\n";
+      message += "Вік: " + age + "\n";
+
+      if (city === "Київ" || city === "Вашингтон" || city === "Лондон") {
+        let country = "";
+        switch (city) {
+          case "Київ":
+            country = "України";
+            break;
+          case "Вашингтон":
+            country = "США";
+            break;
+          case "Лондон":
+            country = "Великої Британії";
+            break;
+        }
+        message += "Ти живеш у столиці " + country + ".";
+      } else {
+        message += "Ти живеш у місті " + city + ".";
+      }
+
+      let champion = "";
+      switch (sport) {
+        case "футбол":
+          champion = "Кріштіано Роналдо";
+          break;
+        case "теніс":
+          champion = "Роджер Федерер";
+          break;
+        case "баскетбол":
+          champion = "Майкл Джордан";
+          break;
+      }
+
+      if (champion) {
+        message += "\n\nКруто! Хочеш стати як " + champion + "?";
+      }
+
+      alert(message);
+    }
+  }
+}
